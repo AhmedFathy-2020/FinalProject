87abb4fd5e146ca59bbefe036ad846b785cc320c
AhmedFathy-2020 <101827022+AhmedFathy-2020@users.noreply.github.com>
2022-03-24 21:57:35 +0200

    Add files via upload

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,18 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\Flutter MOP\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "C:\\Users\\AHMED\\Desktop\\moppApp\\project\\Genius Clash" PROJECT_DIR)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\Flutter MOP\\flutter"
+  "PROJECT_DIR=C:\\Users\\AHMED\\Desktop\\moppApp\\project\\Genius Clash"
+  "FLUTTER_ROOT=C:\\Flutter MOP\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=C:\\Users\\AHMED\\Desktop\\moppApp\\project\\Genius Clash\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=C:\\Users\\AHMED\\Desktop\\moppApp\\project\\Genius Clash"
+  "FLUTTER_TARGET=C:\\Users\\AHMED\\Desktop\\moppApp\\project\\Genius Clash\\lib\\main.dart"
+  "DART_DEFINES=Zmx1dHRlci5pbnNwZWN0b3Iuc3RydWN0dXJlZEVycm9ycz10cnVl,RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=C:\\Users\\AHMED\\Desktop\\moppApp\\project\\Genius Clash\\.dart_tool\\package_config.json"
+)
